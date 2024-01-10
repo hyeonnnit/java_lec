cdd361bb95ef6e75733d7ba7ed0a3e5a069ff5e2
hyeon <lhyeonnit@gmail.com>
2024-01-10 15:36:44 +0900

    ã전이중

--- src/ex/ex17/full/fullEx03/Client.java
+++ src/ex/ex17/full/fullEx03/Client.java
@@ -1,6 +1,8 @@
 package ex.ex17.full.fullEx03;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
@@ -12,6 +14,9 @@ public class Client {
             Socket socket = new Socket("localhost", 2000);
             PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
             Scanner sc = new Scanner(System.in);
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream())
+            );
             // 2. 메세지 전송 스레드
             new Thread(() -> {
                 while (true) {
@@ -21,6 +26,16 @@ public class Client {
             }).start();
 
             //3. 메세지 읽기 스레드
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        String requestMsg = br.readLine();
+                        System.out.println("클라이언트로부터 받은 메세지: " + requestMsg);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/ex/ex17/full/fullEx03/Server.java
+++ src/ex/ex17/full/fullEx03/Server.java
@@ -3,8 +3,10 @@ package ex.ex17.full.fullEx03;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
     public static void main(String[] args) {
@@ -12,6 +14,8 @@ public class Server {
             // 1. 소켓과 버퍼 만들기
             ServerSocket serverSocket = new ServerSocket(2000);
             Socket socket = serverSocket.accept();
+            PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+            Scanner sc = new Scanner(System.in);
             BufferedReader br = new BufferedReader(
                     new InputStreamReader(socket.getInputStream())
             );
@@ -24,6 +28,12 @@ public class Server {
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
+                }
+            }).start();
+            new Thread(() -> {
+                while (true) {
+                    String keyboardMsg = sc.nextLine();
+                    pw.println(keyboardMsg);
                 }
             }).start();
         } catch (IOException e) {
